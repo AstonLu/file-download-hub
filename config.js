6bbdb35488c7288c88be5f88243b19197e8062d2
Aston <jeff200210142@gmail.com>
2026-07-19 18:46:53 +0800

    Add upload configuration

---
+++ config.js
@@ -0,0 +1,5 @@
+window.FILE_HUB_CONFIG = Object.freeze({
+  // 部署 Cloudflare Worker 後，填入完整網址，例如：
+  // uploadApi: "https://file-download-hub-upload.your-subdomain.workers.dev",
+  uploadApi: "",
+});
